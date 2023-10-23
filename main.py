d35bac86cae7e0af4318964a71b10b67d026f49a
ZhekaSemenov <esemenov85@gmail.com>
2023-10-23 16:32:37 +0300

    замена на файл из папки

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import qrcode
-# пример данных
+# вписываем url
 data = "https://index.tatarstan.ru/atp/welcome/"
 # имя конечного файла
 filename = "site.png"
 # генерируем qr-код
 img = qrcode.make(data)
-# сохраняем img в файл
+# сохраняем img в файл в текущей директории
 img.save(filename)
+# указываем папку для сохранения
+path = '/Users/esemenov/'
+# сохраняем qr код в указанную папку
+img.save(path+filename)
 
